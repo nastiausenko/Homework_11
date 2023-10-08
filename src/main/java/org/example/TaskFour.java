67e544cc9e86bb05ab6d290a4389d44d7bffe402
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-08 19:42:59 +0300

    add generate() method

--- src/main/java/org/example/TaskFour.java
+++ src/main/java/org/example/TaskFour.java
@@ -3,6 +3,10 @@ package org.example;
 import java.util.stream.Stream;
 
 public class TaskFour {
+    public Stream<Long> generate(long a, long c, long m, long seed){
+        return Stream.iterate(seed, x -> (a * x + c) % m);
+    }
+
     public static void main(String[] args) {
 
     }
