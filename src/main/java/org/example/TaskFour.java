db76372474709817d271d49efdded20bcf277a80
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2023-10-08 19:46:52 +0300

    complete task four

--- src/main/java/org/example/TaskFour.java
+++ src/main/java/org/example/TaskFour.java
@@ -3,11 +3,18 @@ package org.example;
 import java.util.stream.Stream;
 
 public class TaskFour {
-    public Stream<Long> generate(long a, long c, long m, long seed){
+    public static Stream<Long> generate(long a, long c, long m, long seed){
         return Stream.iterate(seed, x -> (a * x + c) % m);
     }
 
     public static void main(String[] args) {
+        long a = 25214903917L;
+        long c = 11L;
+        long m = (long) Math.pow(2, 48);
+        long seed = System.currentTimeMillis();
 
+        Stream<Long> randomStream = generate(a, c, m, seed);
+        randomStream.limit(10)
+                .forEach(System.out::println);
     }
 }
